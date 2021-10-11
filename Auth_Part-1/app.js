1e2249c1bfeb9a3ba241adad53a9cf60300b6929
Morol <morolswediu@gmail.com>
2021-10-11 16:34:28 +0600

    basic node

---
+++ Auth_Part-1/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import appRouter from './router/appRouter';
+
+const app = express();
+
+// middleware
+app
+  .use(express.json()); 
+
+// routes
+app
+  .use('/api', appRouter);
+
+export default app;
